b77f2d805fa5182ac4fb7de060fad410dd360f45
Plofland <ploflan@gmail.com>
2021-06-01 19:52:16 -0400

    Header, Footer, and Main content layout set up

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,49 @@
-import 'tailwindcss/tailwind.css'
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<div className="flex flex-col h-screen">
+			<header className="flex items-center px-4 py-3 border-b">
+				<img
+					src="https://pbs.twimg.com/profile_images/1175804192197361664/BCMBEZcf_400x400.jpg"
+					alt="profile picture"
+					className="rounded-full w-7 h-7"
+				/>
+				<p className="text-lg ml-6 font-extrabold">
+					Latest Tweets
+				</p>
+			</header>
+			<main className="flex-1">
+				<Component {...pageProps} />
+			</main>
+			<footer className="flex border-t">
+				<a
+					className="w-1/4 py-4 text-center"
+					href=""
+				>
+					Link
+				</a>
+				<a
+					className="w-1/4 py-4 text-center"
+					href=""
+				>
+					Link
+				</a>
+				<a
+					className="w-1/4 py-4 text-center"
+					href=""
+				>
+					Link
+				</a>
+				<a
+					className="w-1/4 py-4 text-center"
+					href=""
+				>
+					Link
+				</a>
+			</footer>
+		</div>
+	);
 }
 
-export default MyApp
+export default MyApp;
