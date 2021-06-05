64ea11d090e30f3c43348438b7894aad81e4ff5b
Plofland <ploflan@gmail.com>
2021-06-04 22:01:00 -0400

    Mock notifications header built

--- pages/notifications.js
+++ pages/notifications.js
@@ -1,5 +1,18 @@
 function Notifications() {
-	return <p className="p-4">Notifications</p>;
+	return (
+		<div className="flex border-b border-gray-200">
+			<div className="flex-1 border-b-2 border-blue-500">
+				<p className="text-center font-bold py-3 text-sm text-blue-500">
+					All
+				</p>
+			</div>
+			<div className="flex-1 border-b">
+				<p className="text-center font-bold py-3 text-sm text-gray-500">
+					Mentions
+				</p>
+			</div>
+		</div>
+	);
 }
 
 Notifications.headerTitle = 'Notifications';
